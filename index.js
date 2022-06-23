d80c631a464bc38fff06b8cd15850882a5fbf51f
Lornah Mukiri <mukirikabuli@gmail.com>
2022-06-23 19:39:12 +0300

    Done

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 // code your solution here
+function superbowlWin(record){
+
+    let result =  record.find((record) =>
+   record.result === "W" ? record.year : undefined
+   )
+   return result!= undefined? result.year:undefined;
+   }
